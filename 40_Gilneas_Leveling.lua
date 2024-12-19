3c2282acc9a24b3163347d26c46dd2b09a4b9f43
ldmz <163733100+Lotris-wow@users.noreply.github.com>
2024-12-20 01:00:21 +0200

    first commit

---
+++ 40_Gilneas_Leveling.lua
@@ -0,0 +1,138 @@
+TourGuide:RegisterGuide("Gilneas Leveling", nil, "Horde", function()
+
+return [[
+
+N Level 40 Required |N|You need to be at least level 40 to continue this guide|
+R Ebonmere Farm
+
+A Ebonmere Worgen Infestation |QID|40978| 
+A Ebonmere Bat Infestation |QID|40977| 
+
+C Ebonmere Bat Infestation |QID|40977| 
+
+N Now you can kill worgens for Nighthowl Pelts here
+N Go South-West
+N Now you can kill worgens for Bloodclaw Pelts here
+
+C Ebonmere Worgen Infestation |QID|40978| 
+
+T Ebonmere Worgen Infestation |QID|40978|
+T Ebonmere Bat Infestation |QID|40977| 
+
+A Ebonmere Affairs |QID|40979| |Z|Gilneas City|
+N This is for dungeon
+
+N Go west to the road
+N Follow the road to some tents near it, stop turn left and follow up to the Stillward Church
+
+R Stillward Church
+A War on Worgen |QID|40976|
+A A Chief Among Brutes |QID|40855|
+A Vernon's Task |QID|40854|
+A Dead Until Dark |QID|40877|
+A Foulhide Mongrels |QID|40992|
+A Through Greater Magic |QID|40993|
+A For the Collection |QID|40991|
+
+N Go to the Glaymore Stead |N|Now go to the North-West part of graveyard, the you can see the road, follow that road up to the building in mountains|
+R Glaymore Stead
+A Judge and the Phantom |QID|40975|
+
+N Follow the road to the south, when when you see the road fork - go right
+N Kill Ogres in the cave direction
+N In the north part of the cave is Chief Maulfist, kill him and loot|
+C A Chief Among Brutes |QID|40855|
+N Keep killing ogres for Vernon's Task, when going out of the cave
+C Vernon's Task |QID|40854|
+N Now go west and kill gnolls
+C Foulhide Mongrels |QID|40992|
+
+N Go west and find Harrison Blackheart, sitting on the part of damaged war machine
+A Greymane Hatred |QID|40980|
+
+N Go north to the road and follow it in north-west direction through the Westgate Tower
+R Westgate Tower
+N Cross the middle bridge and you will see the house in the right direction
+R Ruins of Greyshire
+T Dead Until Dark |QID|40877|
+A All We Need is Blood |QID|40878|
+
+N You can do the dungeon now
+
+N Now go east to crossroad turn left and cross the North bridge
+N At the right you will see the cave |N|enter it and kill Worgens for the War on Worgen|
+C War on Worgen |QID|40976|
+N After that go to Ebonmere Farm, killing all bats you'll see
+R Ebonmere Farm
+C All We Need is Blood |QID|40878|
+T Ebonmere Affairs |QID|40979|
+
+N Now go throght the mountains to Glaymore Stead 
+R Glaymore Stead 
+T Judge and the Phantom |QID|40975|
+N follow up to the Stillward Church
+R Stillward Church
+
+T War on Worgen |QID|40976|
+T A Chief Among Brutes |QID|40855|
+T Vernon's Task |QID|40854|
+
+N Go west and find Harrison Blackheart, sitting on the part of damaged war machine
+T Greymane Hatred |QID|40980|
+
+A The Blackheart Killer |QID|40981|
+N Go west to the bridge, after it, follow the road to the next bridge
+N Turn left on the crossroads and follow the road in north-west direction to Blackthorn's Camp
+
+R Blackthorn's Camp 
+A Report to Livia Strongarm |QID|40846|
+A Report to Luke Agamand |QID|40844|
+T Report to Luke Agamand |QID|40844|
+
+A A Royal Heist |QID|41113| |Z|Gilneas City|
+A Heist in Dryrock Mine |QID|40845|
+A Forsaken at the Church |QID|40851|
+
+T Report to Livia Strongarm |QID|40846|
+A Rendezvous with the Infiltrator |QID|40847|
+
+N Go back folllowing the road, turn right on crossroads
+C Heist in Dryrock Mine |QID|40845|
+N Go back and turn it in |QID|40845|
+T Heist in Dryrock Mine |QID|40845|
+
+R Ruins of Greyshire
+T All We Need is Blood |QID|40878|
+A Last of the Living Dead |QID|40879|
+C Last of the Living Dead |QID|40879|
+T Last of the Living Dead |QID|40879|
+A We Take It From The Living |QID|40880|
+N Go to the Stillward Church killing worgens you will see
+C We Take It From The Living |QID|40880|
+
+R Stillward Church
+T Forsaken at the Church |QID|40851|
+T Foulhide Mongrels |QID|40992|
+
+C Guns of Gilneas |QID|41122| |N|Cannoneer Rileson slain|
+C The Blackheart Killer |QID|40981|
+
+N Go west and find Harrison Blackheart, sitting on the part of damaged war machine
+T The Blackheart Killer |QID|40981|
+A In Memory of Franklin |QID|40982|
+C In Memory of Franklin |QID|40982|
+T In Memory of Franklin |QID|40982|
+
+R Ruins of Greyshire
+T We Take It From The Living |QID|40880|
+A The Evil Made Me Do It |QID|40881| |Z|Gilneas City|
+
+T The Evil Made Me Do It |QID|40881| |Z|Gilneas City|
+T A Royal Heist |QID|41113| |Z|Gilneas City|
+
+A Blood for Blood |QID|40882|
+
+N Guide Complete |N|Tick to continue to the next guide|
+
+]]
+end)
---
+++ Danonzo's_Tel'Abim_Bananas.lua
@@ -0,0 +1,34 @@
+TourGuide:RegisterGuide("Danonzo's Tel'Abim Bananas", nil, "Alliance",function()
+
+return [[
+
+N Level 60 Required |N|You need to be at least level 60 to continue this guide
+
+A A Must Have Discovery |N|A Must Have Discovery| |QID|40734| |Z|Tel'Abim|
+C A Must Have Discovery |N|only Gargantuan Banana left, than I must have it (95.3, 36)| |QID|40734| |Z|Tel'Abim|
+T A Must Have Discovery |N|A Must Have Discovery| |QID|40734| |Z|Tel'Abim|
+
+A The Micro Filter Tazzo-Scope |N|Speak with Tazzo Gearfire and accept The Micro Filter Tazzo-Scope (96.5, 66.8)| |QID|40735| |Z|Tel'Abim|
+C The Micro Filter Tazzo-Scope |N|Collect a Star Ruby, a Blue Sapphire, and an Accurate scope for Tazzo Gearfire| |L|7910| |L|12361| |L|4407| |QID|40735| |Z|Tel'Abim|
+T The Micro Filter Tazzo-Scope |N|Turn In The Micro Filter Tazzo-Scope| |QID|40735| |Z|Tel'Abim|
+
+A Gargantuan Studies |N|Speak with Tazzo Gearfire and accept Gargantuan Studies| |QID|40736| |Z|Tel'Abim|
+C Gargantuan Studies |N|Gather 1 Dream Dust, 1 Greater Eternal Essence, 1 Small Radiant Shard, and 5 Ripe Tel'abim Bananas for Tazzo Gearfire| |L|60954| |L|11176| |L|16203| |L|11177| |QID|40736| |Z|Tel'Abim|
+T Gargantuan Studies |N|Turn In Gargantuan Studies (96.5, 66.8)| |QID|40736| |Z|Tel'Abim|
+
+A The Final Test |N|Speak with Tazzo Gearfire and accept The Final Test| |QID|40737| |Z|Tel'Abim|
+C The Final Test |N|Wait for Tazzo Gearfire fo finish his experiments| |QID|40737| |Z|Tel'Abim|
+T The Final Test |N|Turn In The Final Test| |QID|40737| |Z|Tel'Abim|
+
+A News for Danonzo! |N|Speak with Tazzo Gearfire and accept  News for Danonzo!| |QID|40738| |Z|Tel'Abim|
+C News for Danonzo! |N|Bring a Gargantuan Tel'abim Banana to Chef Danonzo Laxjolt (91.1, 66.1)| |QID|40738| |Z|Tel'Abim|
+T News for Danonzo! |N|Turn In News for Danonzo! (91.1, 66.1)| |QID|40738| |Z|Tel'Abim|
+
+A Danonzo's Tel'Abim Surprise |N|Speak with Chef Danonzo Laxjolt and accept Danonzo's Tel'Abim Surprise| |QID|40752| |Z|Tel'Abim|
+A Danonzo's Tel'Abim Delight |N|Speak with Chef Danonzo Laxjolt and accept Danonzo's Tel'Abim Delight| |QID|40753| |Z|Tel'Abim|
+A Danonzo's Tel'Abim Medley |N|Speak with Chef Danonzo Laxjolt and accept Danonzo's Tel'Abim Medley| |QID|40754| |Z|Tel'Abim|
+
+N Guide Complete
+
+]]
+end)
